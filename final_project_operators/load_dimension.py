628b033c9854d5556b494b0e48023b796d64a3f9
karllabatistaa <kblb@icomp.ufam.edu.br>
2024-10-05 17:38:59 -0400

    implement LoadDimension Operators to user table

--- final_project_operators/load_dimension.py
+++ final_project_operators/load_dimension.py
@@ -1,6 +1,7 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from util.sql_statements import SQL_TRUNCATE_TABLE,SQL_LOAD_USER_DIM_TABLE
 
 class LoadDimensionOperator(BaseOperator):
 
@@ -8,15 +9,27 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 insert_mode="truncate-insert",
+                 table ="",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id
+        self.insert_mode = insert_mode
+        self.table = table
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info("Starting LoadDimensionOperator ..")
+
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        if self.insert_mode == "truncate-insert":
+            sql_truncate_insert = SQL_TRUNCATE_TABLE.format(table=self.table)
+            redshift.run(sql_truncate_insert)    
+            redshift.run(SQL_LOAD_USER_DIM_TABLE)
+        
+        elif self.insert_mode =="append-only":
+            redshift.run(SQL_LOAD_USER_DIM_TABLE)
+
+        self.log.info('StageToRedshiftOperator DONE!')    
