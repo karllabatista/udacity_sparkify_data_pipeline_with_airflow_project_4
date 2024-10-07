1132ce2f39ded32711a9c073283d738d4130573d
karllabatistaa <kblb@icomp.ufam.edu.br>
2024-10-06 20:22:36 -0400

    impements LoadDimensionOperator

--- final_project_operators/load_dimension.py
+++ final_project_operators/load_dimension.py
@@ -1,7 +1,7 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
-from util.sql_statements import SQL_TRUNCATE_TABLE,SQL_LOAD_USER_DIM_TABLE
+from util.sql_statements import SQL_TRUNCATE_TABLE,SQL_LOAD_USER_DIM_TABLE,SQL_LOAD_SONGS_DIM_TABLE,SQL_LOAD_ARTISTS_DIM_TABLE,SQL_LOAD_TIME_DIM_TABLE
 
 class LoadDimensionOperator(BaseOperator):
 
@@ -23,13 +23,46 @@ class LoadDimensionOperator(BaseOperator):
         self.log.info("Starting LoadDimensionOperator ..")
 
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        if self.table == "users":
+            self.insert_data_by_sql(redshift,
+                                    self.table,
+                                    SQL_LOAD_USER_DIM_TABLE,
+                                    self.insert_mode)
+
+        elif self.table == "songs":
+            self.insert_data_by_sql(redshift,
+                                self.table,
+                                SQL_LOAD_SONGS_DIM_TABLE,
+                                self.insert_mode)
         
-        if self.insert_mode == "truncate-insert":
-            sql_truncate_insert = SQL_TRUNCATE_TABLE.format(table=self.table)
-            redshift.run(sql_truncate_insert)    
-            redshift.run(SQL_LOAD_USER_DIM_TABLE)
-        
-        elif self.insert_mode =="append-only":
-            redshift.run(SQL_LOAD_USER_DIM_TABLE)
+        elif self.table == "artists":
+            self.insert_data_by_sql(redshift,
+                                self.table,
+                                SQL_LOAD_ARTISTS_DIM_TABLE,
+                                self.insert_mode)
+            
+        elif self.table == "time":
+            self.insert_data_by_sql(redshift,
+                                self.table,
+                                SQL_LOAD_TIME_DIM_TABLE,
+                                self.insert_mode)
+            
 
         self.log.info('StageToRedshiftOperator DONE!')    
+    
+    def insert_data_by_sql(self,sql_helper,table_name,sql_statement,insert_mode):
+            
+            self.log.info(f"Inserting data on {table_name} ..")    
+    
+            
+            if insert_mode == "truncate-insert":
+                sql_truncate_insert = SQL_TRUNCATE_TABLE.format(table=table_name)
+                sql_helper.run(sql_truncate_insert)    
+                sql_helper.run(sql_statement)
+        
+            elif insert_mode =="append-only":
+                sql_helper.run(sql_statement)
+
+
+            self.log.info(f"Insert data on {table_name} finished!")    
