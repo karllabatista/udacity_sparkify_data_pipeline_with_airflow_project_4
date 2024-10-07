e090569375b2b10888a3f43dc41fd44de7879adb
karllabatistaa <kblb@icomp.ufam.edu.br>
2024-10-06 20:21:56 -0400

    active and insert dependecy btw taks

--- final_project.py
+++ final_project.py
@@ -6,7 +6,7 @@ from airflow.operators.dummy_operator import DummyOperator
 from final_project_operators.stage_redshift import StageToRedshiftOperator
 from final_project_operators.load_fact import LoadFactOperator
 from final_project_operators.load_dimension import LoadDimensionOperator
-#from final_project_operators.data_quality import DataQualityOperator
+from final_project_operators.data_quality import DataQualityOperator
 
 
 
@@ -62,29 +62,42 @@ def final_project():
         insert_mode="truncate-insert",
         table="users" 
     )
-    ''''
+    
     load_song_dimension_table = LoadDimensionOperator(
         task_id='Load_song_dim_table',
+        redshift_conn_id="redshift_conn",
+        insert_mode="truncate-insert",
+        table="songs" 
     )
-
+    
     load_artist_dimension_table = LoadDimensionOperator(
         task_id='Load_artist_dim_table',
+        redshift_conn_id="redshift_conn",
+        insert_mode="truncate-insert",
+        table="artists" 
     )
-
+    
     load_time_dimension_table = LoadDimensionOperator(
         task_id='Load_time_dim_table',
+        redshift_conn_id="redshift_conn",
+        insert_mode="truncate-insert",
+        table="time" 
     )
-
+    '''
     run_quality_checks = DataQualityOperator(
         task_id='Run_data_quality_checks',
     )
-
-    
     '''
+    
+    
     start_operator >> stage_events_to_redshift 
     start_operator >> stage_songs_to_redshift
     stage_events_to_redshift >> load_songplays_table
     stage_songs_to_redshift >> load_songplays_table
     load_songplays_table >> load_user_dimension_table
+    load_songplays_table >> load_song_dimension_table
+    load_songplays_table >> load_artist_dimension_table
+    load_songplays_table >> load_time_dimension_table
+
 
 final_project_dag = final_project()
